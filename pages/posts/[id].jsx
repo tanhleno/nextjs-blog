3667319ac7516ee4415f02656082f8523d110c45
Tanhleno Teixeira de Sousa <tts.tanhleno@gmail.com>
2023-05-14 12:20:15 -0300

    Polish the post page

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Layout from '../../components/layout';
 import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
   return (
@@ -9,9 +10,15 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <h1>{postData.title}</h1>
-      <Date dateString={postData.date} />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>
+          {postData.title}
+        </h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article> 
     </Layout>
   );
 }
