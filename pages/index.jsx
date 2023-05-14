34de81dfdc5dff38da95e0242ecf3803518387a2
Tanhleno Teixeira de Sousa <tts.tanhleno@gmail.com>
2023-05-14 12:27:28 -0300

    Polish the home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
+import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
@@ -22,13 +23,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
-              <br />
-              <Link href={`posts/${id}`}>Access here</Link>
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
